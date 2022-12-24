c5d7bf7a17c52af07408929c145169653d671664
Tim Strazzere <tim@strazzere.com>
2022-12-24 04:22:23 -0800

    Looking for the integrity checks

--- agent/index.ts
+++ agent/index.ts
@@ -12,6 +12,45 @@ const getStack = () => {
 let hooked: boolean = false;
 
 function hookDexHelper() {
+  // LOAD:000000000003B484 loc_3B484                               ; CODE XREF: sub_385BC+2EA0↑j
+  // LOAD:000000000003B484                 LDR             X0, [X29,#0x108]
+  // LOAD:000000000003B488                 MOV             X2, #0x10 ; size_t
+  // LOAD:000000000003B48C                 MOV             W19, #0
+  // LOAD:000000000003B490                 LDR             X1, [X0,#0x9C0] ; void *
+  // LOAD:000000000003B494                 MOV             X0, X25 ; void *
+  // LOAD:000000000003B498                 BL              .memcmp
+  // hook the above specific call to .memcmp and inspect the contents
+  // this is likely the md5 (?) check against the original classes.dex for integrity check, I think?
+  const memcmp = Module.findBaseAddress('libDexHelper.so')?.add(0x0003b498);
+  if (memcmp) {
+    log('[*] hooked specific memcmp : ', memcmp);
+    Interceptor.attach(memcmp, {
+      onEnter: function (args) {
+        log('specific memcmp');
+        console.log(
+          hexdump(args[0], {
+            offset: 0,
+            length: args[2].toInt32(),
+            header: true,
+            ansi: true,
+          }),
+        );
+        console.log(
+          hexdump(args[1], {
+            offset: 0,
+            length: args[2].toInt32(),
+            header: true,
+            ansi: true,
+          }),
+        );
+        log(Stack.native(this.context));
+      },
+      onLeave: function (retval) {
+        log(`memcmp equal ret: ${retval.toInt32()}`);
+      },
+    });
+  }
+
   // 00000000000CE44C                 EXPORT zipOpen
   const zipOpen = Module.findExportByName('libDexHelper.so', 'zipOpen');
   if (zipOpen) {
