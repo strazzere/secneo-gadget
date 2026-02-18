c515935f870b1945a3078259cd159527c9d23b9e
Tim Strazzere <tim@strazzere.com>
2026-02-18 12:35:29 -0800

    chore: updating frida gum type resolved this issue

--- agent/dex.ts
+++ agent/dex.ts
@@ -3,14 +3,11 @@ import { log } from "./logger.js";
 export function getPackageName(): string | undefined {
   const pid = Process.id;
 
-  // @ts-expect-error
   const cmdline = new File(`/proc/${pid}/cmdline`, "rb");
 
-  // @ts-expect-error
   let packageName: string | undefined = cmdline.readLine();
   log(`packageName: ${packageName}`);
 
-  // @ts-expect-error
   cmdline.close();
 
   if (packageName?.includes(":")) {
@@ -29,11 +26,8 @@ export function writeDexToFile(address: NativePointer) {
     log(`[*] Writing dex to ${fileName}`);
     const dex = address.readByteArray(dexSize);
     if (dex) {
-      // @ts-expect-error
       const out = new File(fileName, "wb");
-      // @ts-expect-error
       out.write(dex);
-      // @ts-expect-error
       out.close();
     }
   } catch (e) {
